16484fae2b7a06e14bc8bf3f88b4b5b5cf6299da
Ankita Tripathi <ankitatripathi.2312@gmail.com>
2022-02-07 20:59:11 +0530

    updating dates.ts

--- app/src/plugins/dates.ts
+++ app/src/plugins/dates.ts
@@ -17,7 +17,7 @@
 const DAY_MS = 24 * 60 * 60 * 1000;
 
 export function renderDaysAgo(lastUpdated: number) {
-  const now = new Date().getTime();
+  const now = new Date().getMonth();
 
   const diff = now - lastUpdated;
   const daysAgo = Math.floor(diff / DAY_MS);
@@ -26,7 +26,12 @@ export function renderDaysAgo(lastUpdated: number) {
     return "today";
   } else if (daysAgo === 1) {
     return "yesterday";
-  } else {
+  } 
+  else if(daysAgo <= 31){
     return `${daysAgo} days ago`;
+  }
+  else
+  {
+    return `${now} months ago`;
   }
 }
